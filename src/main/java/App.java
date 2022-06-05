9cf28d508fa917fbf2925779db0f4a39a7c92fe8
ViktorCherepennikov <viktorcherepennikov@gmail.com>
2022-06-05 14:29:48 +0300

    Commit1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getMessage());
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+        boolean helloWorldEquals = bean == bean2;
+        boolean catsEquals = cat1 == cat2;
+        System.out.println(helloWorldEquals + " - для  HelloWorld \n" + catsEquals + " - для Cat" );
+
     }
 }
